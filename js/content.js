185f07c895961ff781674e0463847b994f91a928
amund berge <amunddb@stud.ntnu.no>
2024-07-11 23:36:13 +0200

    put content in separate file

---
+++ js/content.js
@@ -0,0 +1,66 @@
+const aboutContent = `
+<h1>About</h1>
+<p>My name is Amund Dahlmo Berge, and I'm a 20-year-old Norwegian CS student. My interests include
+programming, piano, sports, and chess. BergeOS is my personal website, intended both as an archive 
+of various aspects of my life, as well as displaying these to the rest of the world.</p>
+<p>As life goes on, I want to post monthly life updates here, review various 
+media I consume, as well as include my writings on various topics.</p>
+`
+
+const toDoContent = `
+<h1>To-Do:</h1>
+<ul>
+    <li>Style windows properly</li>
+    <li>Write and implement the content for all the icons</li>
+    <li>Create taskbar functionality</li>
+    <li>Find better icons</li>
+    <li>Implement choosing fonts and backgrounds</li>
+    <li>Create guestbook</li>
+    <li>Improve and finalize window functionality</li>
+    <li>Embed the music playlist here instead of linking to YouTube</li>
+    <li>Spend some time fixing stuff rather than writing an endless to-do list</li>
+</ul> 
+`
+
+const projectsContent = `
+<h1>Projects</h1>
+<p>Well, I currently don't have any real projects to show for. 
+Except for this site, I suppose. Some projects I plan to do in the near future are: </p>
+<ul>
+    <li>Chess engine</li>
+    <li>HTTP messaging server</li>
+    <li>Piano cover of "Killing My Love"</li>
+</ul>
+<p>Though the list is brief for now, my planned projects are not limited to programming.
+I also want to do some lifestyle experiments, such as: </p>
+<ul>
+    <li>To go some period without listening to music</li>
+    <li>To go some period using my phone and computer only for basic communication, school, other essentials, and nothing else</li>
+</ul>
+`
+const chessContent = `
+<h1>Play me in chess!</h1>
+<p>This window will be used for playing against my chess engine once it has been made.</p> 
+<p>If you would like to play chess with me instead, you can send me a challenge here:
+<a href="https://lichess.org/@/Mr_Berge" target="_blank">MyLichessAccount</a></p>
+`
+
+const writingsContent = `
+<h1>Writings</h1>
+<p>My future writings will be found here. Some topics I want to explore are music, literature,
+philosophy and religion, and lifestyle. I also want to write reviews of books, movies, series, 
+and even albums that I consume.</p><p>Stay tuned for more!</p>
+`
+
+const guestbookContent = `
+<h1>Guestbook</h1>
+<p>Once implemented, you will be able to leave your own remarks on the site.</p>
+<p>Stay tuned!</p>
+`
+
+const blogContent = `
+<h1>Blog</h1>
+<p>On my blog, I will give monthly updates where I share what I did, what I consumed,
+and what I learned.</p>
+<p>Coming soon!</p>
+`
--- js/main.js
+++ js/main.js
@@ -7,44 +7,6 @@ if (rootURL.includes('localhost')){    // simple fix for running the project loc
     rootURL = '..'; 
 }
 
-const aboutContent = `
-<h1>About</h1>
-<p>My name is Amund Dahlmo Berge, and I'm a 20-year-old Norwegian CS student. My interests include
-programming, piano, sports, and chess. BergeOS is my personal website, intended both as an archive 
-of various aspects of my life, as well as displaying these to the rest of the world.</p>
-<p>As life goes on, I want to post monthly life updates here, review various 
-media I consume, as well as include my writings on various topics.</p>
-`
-
-const toDoContent = `
-<h1>To-Do:</h1>
-<ul>
-    <li>Style windows properly</li>
-    <li>Write and implement the content for all the icons</li>
-    <li>Create taskbar functionality</li>
-    <li>Find better icons</li>
-    <li>Implement choosing fonts and backgrounds</li>
-    <li>Create guestbook</li>
-</ul> 
-`
-
-const projectsContent = `
-<h1>Projects</h1>
-<p>Well, I currently don't have any real projects to show for. 
-Except for this site, I suppose. Some projects I plan to do in the near future are: </p>
-<ul>
-    <li>Chess engine</li>
-    <li>HTTP messaging server</li>
-    <li>Piano cover of "Killing My Love"</li>
-</ul>
-<p>Though the list is brief for now, my planned projects are not limited to programming.
-I also want to do some lifestyle experiments, such as: </p>
-<ul>
-    <li>To go some period without listening to music</li>
-    <li>To go some period using my phone and computer only for basic communication, school, other essentials, and nothing else</li>
-</ul>
-`
-
 function getCurrentDateTime(){
     return new Date().toLocaleString('de-DE');
 }
@@ -65,6 +27,11 @@ about.addEventListener('dblclick', function(){
     createWindow(about.id, aboutContent);
 })
 
+let writings = document.querySelector('#writings');
+writings.addEventListener('dblclick', function(){
+    createWindow(writings.id, writingsContent);
+})
+
 let toDo = document.querySelector('#to-do');
 toDo.addEventListener('dblclick', function(){
     createWindow(toDo.id, toDoContent);
@@ -72,12 +39,22 @@ toDo.addEventListener('dblclick', function(){
 
 let chess = document.querySelector('#chess');
 chess.addEventListener('dblclick', function(){
-    main.style.backgroundColor = 'lightblue';
+    createWindow(chess.id, chessContent);
+})
+
+let guestbook = document.querySelector('#guestbook');
+guestbook.addEventListener('dblclick', function(){
+    createWindow(guestbook.id, guestbookContent);
 })
 
 let projects = document.querySelector('#projects')
 projects.addEventListener('dblclick', function(){
     createWindow(projects.id, projectsContent);
+})
+
+let blog = document.querySelector('#blog');
+blog.addEventListener('dblclick', function(){
+    createWindow(blog.id, blogContent);
 })
 
 let main = document.querySelector('#main');
